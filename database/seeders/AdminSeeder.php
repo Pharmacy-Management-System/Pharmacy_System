2e7f8a5e8a4ea2663146d65e25a63078ab3d572b
Radwa99 <radwa.prof.9@gmail.com>
2023-03-27 22:34:56 +0200

    admin seeder and areas migration

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
+
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            AdminSeeder::class,
+        ]);
     }
 }
