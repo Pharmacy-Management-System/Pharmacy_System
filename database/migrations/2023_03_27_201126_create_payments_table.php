6488b0bad252d93b2425798ec3c98795f51ce6b3
Mariam Reda <mariamreda1819@gmail.com>
2023-03-27 22:34:30 +0200

    Create Medicines and Payments

---
+++ database/migrations/2023_03_27_201126_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->enum('method', ['cash', 'card']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
