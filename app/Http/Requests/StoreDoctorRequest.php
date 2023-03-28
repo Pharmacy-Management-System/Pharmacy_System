304f7652669665317f0b97045a7672f66b9c7324
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-03-28 10:34:48 +0200

    Doctors Table Validation

---
+++ app/Http/Requests/StoreDoctorRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'national_id'=> ['required','size:14','unique:doctors,national_id,'.$this->doctor],
+            'email'=> ['required','email','unique:doctors,email,'.$this->doctor],
+            'name' => ['required'],
+            'password' => ['required','min:6'],
+            'avatar' => ['mimes:jpg,jpeg'],
+            'pharmacy_id' => ['required|exists:pharmacies,pharmacy_id'],
+            'is_banned' => ['required']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'national_id' => [
+                'required' => 'The National ID is Required',
+                'unique' => 'The National ID must be Unique',
+                'size' => 'The National ID must Contain 14 Number'
+            ],
+            'email' => [
+                'required' => 'The Email is Required',
+                'unique' => 'The Email must be Unique',
+                'email' => 'The Email must be a valid Email'
+            ],
+            'name' => [
+                'required' => 'The Name is Required'
+            ],
+            'password' => [
+                'required' => 'The Password is Required',
+                'min' => 'The Password must be larger than 6 Characters'
+            ],
+            'avatar' => [
+                'mimes' => 'An Image must be jpg or jpeg Only'
+            ]
+        ];
+    }
+}
