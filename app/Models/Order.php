0c8444ae805d02b203abf2543ff212831d4df1cf
rowan <rowantamer5@gmail.com>
2023-03-28 18:04:02 +0200

    order model

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'order_creator',
+        'doctor_name',
+        'delivering_address',
+        'pharmacy_name',
+        'is_insured',
+        'status',
+        'creator_type',
+        'quantity',
+    ];
+}
