2f64e0c594bddc0a62184f0fce4149e9e64b6235
Mariam Reda <mariamreda1819@gmail.com>
2023-03-28 13:51:32 +0200

    Vaildation Request for Medicine and Payment

---
+++ app/Http/Requests/StoreMedicineRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMedicineRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'id'=> ['required','unique:medicines,id'],
+            'name' => ['required','string'],
+            'type' => ['required','string'],
+            'quantity' => ['required','integer','min:1'],
+            'price' => ['required','double']
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'id' => [
+                'required' => 'Medicine ID is Required',
+                'unique' => 'Medicine ID must be Unique',
+            ],
+            'name' => [
+                'required' => 'Name is Required'
+            ],
+            'type' => [
+                'required' => 'Type is Required'
+            ],
+            'quantity' => [
+                'required' => 'Quantity is Required',
+                'integer' => 'Quantity must be an integer number',
+                'min' => 'Quantity must be at least 1'
+            ],
+            'price' => [
+                'required' => 'Price is Required',
+            ]
+        ];
+    }
+}
+
---
+++ app/Http/Requests/StorePaymentRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'id'=> ['required','unique:payments,id'],
+            'method' => ['required,in:cash,card'],
+            'order_id' => ['required|exists:orders,id'],
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'id' => [
+                'required' => 'Payment ID is Required',
+            ],
+            'method' => [
+                'required' => 'Payment method is Required',
+                'in' => 'Payment method must be cash or card',
+            ]
+        ];
+    }
+}
