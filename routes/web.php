e967b8b85f3724fe63709a1cb9858a987e23779f
rowan <rowantamer5@gmail.com>
2023-03-30 22:55:48 +0200

    edit data of medicine

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AreaController;
 use App\Http\Controllers\ClientController;
-
+use App\Http\Controllers\MedicineController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
