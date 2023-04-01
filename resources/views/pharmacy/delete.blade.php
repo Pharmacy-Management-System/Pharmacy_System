3885f8bba85981620b14d5f85d73c5cbe89b8ea7
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-01 23:41:31 +0200

    Restore Pharmacy

--- resources/views/pharmacy/delete.blade.php
+++ resources/views/pharmacy/delete.blade.php
@@ -19,28 +19,6 @@
     </div>
 </div>
 
-<!-- restore moadal -->
-<div class="modal fade" id="restorePharmacyModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-light" id="staticBackdropLabel">
-                    <i class="bi bi-exclamation-triangle-fill text-success me-2"></i>Warning
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <h5>Are You Sure to Restore this Record?</h5>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button id="restore" type="button" class="btn btn-success">Restore</button>
-            </div>
-
-        </div>
-    </div>
-</div>
-
 <!--Script-->
 <script>
     function showDeleteModal(event) {
--- resources/views/pharmacy/restore.blade.php
+++ resources/views/pharmacy/restore.blade.php
@@ -1,10 +1,10 @@
-<!-- restore moadal -->
+<!-- Restore Pharmacy Modal -->
 <div class="modal fade" id="restorePharmacyModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title text-light" id="staticBackdropLabel">
-                    <i class="bi bi-exclamation-triangle-fill text-success me-2"></i>Warning
+                    <i class="bi bi-exclamation-triangle-fill text-success me-2"></i>Confirmation
                 </h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
