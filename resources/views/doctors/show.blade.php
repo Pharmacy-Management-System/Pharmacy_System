2670bb921dcc974fa95b808abcf6bdf4a54d0806
Radwa99 <radwa.prof.9@gmail.com>
2023-04-01 15:45:44 +0200

    edit show

--- resources/views/doctors/show.blade.php
+++ resources/views/doctors/show.blade.php
@@ -1,14 +1,14 @@
 <!-----------Show modal------------>
 <div class="modal fade" id="show-doctor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Doctor Info</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <img id="doctorImage" src="" alt="Doctor Image">
+            <div class="modal-body d-flex flex-column justify-content-center align-items-center">
+                <div class="mb-3 flex-column justify-content-center align-items-center">
+                    <img id="doctorImage" src="" alt="Doctor Image" class="animation__wobble img-circle elevation-2" height="100" width="100">
                 </div>
                 <div class="mb-3">
                     <p><strong>Name:</strong> <span id="doctorName"></span></p>
