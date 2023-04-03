72cceceab74ac306c51b3311c776978812855664
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-03 20:53:50 +0200

    Update Revenue

--- resources/views/revenue/pharmacy.blade.php
+++ resources/views/revenue/pharmacy.blade.php
@@ -1,11 +1,29 @@
 <!--Revenue Pharmacy View-->
 <div class="container">
-    <div class="card p-3">
-        <img src="storage/pharmacies_Images/avatar1.jpg" class="card-img-center img-circle" width="60px">
-        <div class="card-body">
-        <h5 class="card-title">{{$pharmacy['id']}}</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="d-flex justify-content-center align-items-center" style="height:70vh;">
+        <div class="card col-md-8 col-lg-6 p-3 animation__wobble">
+            <div class="d-flex flex-row border-container">
+                <div><img src="storage/pharmacies_Images/{{$pharmacy['avatar_image']}}" class="img-circle" width="60px"></div>
+                <div class="card-title align-middle ml-3 mt-3">{{$pharmacy['pharmacy_name']}}</div>
+            </div>
+
+            <div class="card-body">
+                <ul class="nav nav-pills nav-sidebar flex-column gap-1" data-widget="treeview" role="menu" data-accordion="false" style="font-family: nunito;letter-spacing:1px;">
+                    <li class="nav-item sidebar-list">
+                        <img src="dist/img/icons/ID-icon.png" class="nav-icon">
+                        <strong>ID: </strong><span>{{$pharmacy['id']}}</span>
+                    </li>
+                    <li class="nav-item sidebar-list">
+                        <img src="dist/img/icons/Orders-icon.png" class="nav-icon">
+                        <strong>Total Orders: </strong><span>{{$orders}}</span>
+                    </li>
+                    <li class="nav-item sidebar-list">
+                        <img src="dist/img/icons/Revenue-icon.png" class="nav-icon">
+                        <strong>Total Revenue: </strong><span>{{$revenues}}</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
+
