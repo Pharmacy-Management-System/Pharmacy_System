151324344f97405f9a59ec109ce427c67a0ee658
rowan <rowantamer5@gmail.com>
2023-04-04 13:50:11 +0200

    make order api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
+use App\Models\Area;
 use App\Models\Order;
 use App\Models\OrderMedicine;
 use App\Models\Prescription;
