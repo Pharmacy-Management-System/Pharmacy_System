ca95fd7eee8452305ba454d234fdc5d214dcb112
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-04-06 18:03:16 +0200

    Update Order Status Charts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,10 @@
 @endsection
 
 @section('content')
-<div class="col-12 d-flex justify-content-between align-items-center flex-wrap" style="height: 80vh;">
+<div class="border-white" style="width:30%;margin-left:35%;">
+    <marquee behavior="scroll" direction="left" class="text-center text-monospace fs-2">WELCOME TO ORDERS CHARTS</marquee>
+</div>
+<div class="col-12 d-flex justify-content-between align-items-center flex-wrap" style="height: 70vh;">
     @include('statistics.statusbarchart')
     @include('statistics.statuspiechart')
 </div>
